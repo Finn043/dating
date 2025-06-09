d2714cc81677c2320d46f98d2650c2cc84087119
Finn043 <tin.bao.luong@gmail.com>
2025-06-09 20:27:45 +0700

    script update

--- script.js
+++ script.js
@@ -1,3 +1,14 @@
-function goToPage(page) {
-  window.location.href = page;
+function sendConfirmation(restaurant) {
+  fetch("https://script.google.com/macros/s/AKfycbwQNFbpg3D7-h8RbM_VVzqeD1jqjTGqPyWH_fqY_cjEw-gKCiq5mhfHiseZgawxBumx-w/exec", {
+    method: "POST",
+    mode: "no-cors",
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify({ restaurant })
+  }).then(() => {
+    alert("Đã gửi xác nhận thành công 🥰");
+    window.location.href = "finish.html";
+  }).catch((error) => {
+    alert("Lỗi khi gửi xác nhận 😥");
+    console.error(error);
+  });
 }
